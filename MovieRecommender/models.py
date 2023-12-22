173a8d1cd31b3975e01656bf635ba59ca5f68ab5
GerritBartels <gbartels@uos.de>
2023-12-22 10:49:49 +0100

    Added doc-strings

--- MovieRecommender/models.py
+++ MovieRecommender/models.py
@@ -1,12 +1,14 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask_user import UserMixin
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
 
 db = SQLAlchemy()
 
-# Define the User data-model.
-# NB: Make sure to add flask_user UserMixin as this adds additional fields and properties required by Flask-User
 class User(db.Model, UserMixin):
+    """User account model. Stores user information and authentication details. 
+    Has a one-to-many relationship with MovieTags and MovieRatings.
+    """
+
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
@@ -25,6 +27,10 @@ class User(db.Model, UserMixin):
 
 
 class Movie(db.Model):
+    """Movie model. Stores movie title and publication year.
+    Has a one-to-many relationship with MovieGenre, MovieLinks, MovieTags, and MovieRatings.
+    """
+
     __tablename__ = 'movies'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100, collation='NOCASE'), nullable=False, unique=True)
@@ -36,6 +42,8 @@ class Movie(db.Model):
 
 
 class MovieGenre(db.Model):
+    """Movie genre model. Stores movie id and genres."""
+
     __tablename__ = 'movie_genres'
     id = db.Column(db.Integer, primary_key=True)
     movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
@@ -43,6 +51,8 @@ class MovieGenre(db.Model):
 
 
 class MovieLinks(db.Model):
+    """Movie links model. Stores movie id, imdb id, and tmdb id used for linking to these external."""
+
     __tablename__ = 'movie_links'
     id = db.Column(db.Integer, primary_key=True)
     movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False, unique=True)
@@ -51,6 +61,10 @@ class MovieLinks(db.Model):
 
 
 class MovieTags(db.Model):
+    """Movie tags model. Stores user id, movie id, and tags.
+    A UniqueConstraint is used to prevent duplicate tags for a given user and movie.
+    """
+
     __tablename__ = "movie_tags"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
@@ -62,6 +76,10 @@ class MovieTags(db.Model):
 
 
 class MovieRatings(db.Model):
+    """Movie ratings model. Stores user id, movie id, and ratings.
+    A UniqueConstraint is used to prevent duplicate ratings for a given user and movie.
+    """
+
     __tablename__ = "movie_ratings"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
--- MovieRecommender/read_data.py
+++ MovieRecommender/read_data.py
@@ -1,45 +1,63 @@
 import csv
+import sys
 import datetime
+import flask_user
+import flask_sqlalchemy
 from sqlalchemy.exc import IntegrityError
 from models import User, Movie, MovieGenre, MovieLinks, MovieTags, MovieRatings
 
 
-def check_and_read_data(db, user_manager):
-    # check if we have movies in the database
+def check_and_read_data(db: flask_sqlalchemy.extension.SQLAlchemy, user_manager: flask_user.user_manager.UserManager) -> None:
+    """Reads data from movies, links, tags and ratings csv files and stores them in the database.
+    
+    Arguments:
+        db (flask_sqlalchemy.extension.SQLAlchemy): The SQLAlchemy database object.
+        user_manager (flask_user.user_manager.UserManager): The UserManager object.
+    """
+
+    # Check if we have movies in the database,
     # read data if database is empty
     if Movie.query.count() == 0:
-        # read movies from csv
+        # Read movies from csv
         with open('data/movies.csv', newline='', encoding='utf8') as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             count = 0
+
             for row in reader:
-                if count > 0:
+                if count > 0:       
                     try:
                         id = row[0]
                         title = row[1]
+                        # Year is always part of the title
                         year = title[-5:-1]
                         movie = Movie(id=id, title=title, year=year)
                         db.session.add(movie)
-                        genres = row[2].split('|')  # genres is a list of genres
-                        for genre in genres:  # add each genre to the movie_genre table
+                        # genres is a list of genres
+                        genres = row[2].split('|')  
+
+                        for genre in genres: 
                             movie_genre = MovieGenre(movie_id=id, genre=genre)
                             db.session.add(movie_genre)
-                        db.session.commit()  # save data to database
+
+                        db.session.commit()
+
                     except IntegrityError:
-                        print("Ignoring duplicate movie: " + title)
+                        print("\nIgnoring duplicate movie: " + title)
                         db.session.rollback()
                         pass
+
                 count += 1
                 if count % 100 == 0:
-                    print(count, " movies read")
+                    print('\r' + str(count), " movies read", end="")
 
-        print("Finished reading in movies \n")
+        print("\nFinished reading in movies \n")
+
 
     if MovieLinks.query.count() == 0:
-        # read movie links from csv
         with open('data/links.csv', newline='', encoding='utf8') as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             count = 0
+
             for row in reader:
                 if count > 0:
                     try:
@@ -48,22 +66,25 @@ def check_and_read_data(db, user_manager):
                         tmdb_id = row[2]
                         movie_link = MovieLinks(movie_id=movie_id, imdb_id=imdb_id, tmdb_id=tmdb_id)
                         db.session.add(movie_link)
-                        db.session.commit()  # save data to database
+                        db.session.commit()
+
                     except IntegrityError:
-                        print("Ignoring duplicate movie link: " + movie_id)
+                        print("\nIgnoring duplicate movie link: " + movie_id)
                         db.session.rollback()
                         pass
+
                 count += 1
                 if count % 100 == 0:
-                    print(count, " movie links read")
+                    print('\r' + str(count), " movie links read", end="")
 
-        print("Finished reading in links \n")
+        print("\nFinished reading in links \n")
+
 
     if MovieTags.query.count() == 0:
-        # read movie tags from csv
         with open('data/tags.csv', newline='', encoding='utf8') as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             count = 0
+
             for row in reader:
                 if count > 0:
                     try:
@@ -79,23 +100,25 @@ def check_and_read_data(db, user_manager):
                         timestamp = row[3]
                         movie_tag = MovieTags(user_id=user_id, movie_id=movie_id, tag=tag, timestamp=datetime.date.fromtimestamp(int(timestamp)))
                         db.session.add(movie_tag)
-                        db.session.commit()  # save data to database
+                        db.session.commit()
+
                     except IntegrityError:
-                        print("Integrity error for tag: " + tag + " for movie: " + movie_id + " by user: " + user_id)
+                        print("\nIntegrity error for tag: " + tag + " for movie: " + movie_id + " by user: " + user_id)
                         db.session.rollback()
                         pass
+
                 count += 1
                 if count % 100 == 0:
-                    print(count, " movie tags read")
+                    print('\r' + str(count), " movie tags read", end="")
 
-        print("Finished reading in tags \n")
+        print("\nFinished reading in tags \n")
 
 
     if MovieRatings.query.count() == 0:
-        # read movie ratings from csv
         with open('data/ratings.csv', newline='', encoding='utf8') as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             count = 0
+
             for row in reader:
                 if count > 0:
                     try:
@@ -111,13 +134,20 @@ def check_and_read_data(db, user_manager):
                         timestamp = row[3]
                         movie_rating = MovieRatings(user_id=user_id, movie_id=movie_id, rating=rating, timestamp=datetime.date.fromtimestamp(int(timestamp)))
                         db.session.add(movie_rating)
-                        db.session.commit()  # save data to database
-                    except IntegrityError:
-                        print("Integrity error for rating: " + rating + " for movie: " + movie_id + " by user: " + user_id)
-                        db.session.rollback()
-                        pass
-                count += 1
-                if count % 1000 == 0:
-                    print(count, " movie ratings read")
 
-        print("Finished reading in ratings \n")
+                        # Commit every 2000 rows
+                        if count % 2000 == 0:
+                            db.session.commit()
+                            print('\r' + str(count), " movie ratings read", end="")
+
+                    except IntegrityError:
+                        db.session.rollback()
+                        print("\nIntegrity error for rating: " + rating + " for movie: " + movie_id + " by user: " + user_id)
+                        continue
+
+                count += 1
+
+            # Commit remaining rows
+            db.session.commit()
+
+        print("\nFinished reading in ratings \n")
